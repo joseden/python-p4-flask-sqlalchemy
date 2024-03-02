32d201d84cb0516ba9dabb5302c0aa82a3374999
Joseph <joseph.dena@student.moringaschool.com>
2024-03-02 19:59:28 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+# Set the database URI configuration option
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy and Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Import models after initializing db
+from models import *
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
